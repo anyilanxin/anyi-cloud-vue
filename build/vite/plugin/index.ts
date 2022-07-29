d006cdb1e77df4cb5aab454517a4bffa86f183de
zxiaozhou <z7630853@163.com>
2022-07-29 11:47:19 +0800

    feat: optimize the code and update md
    1. optimize the code
    2. update readme info
    3. Handle the semver-regex vulnerability

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -13,12 +13,10 @@ import { configCompressPlugin } from './compress';
 import { configStyleImportPlugin } from './styleImport';
 import { configVisualizerConfig } from './visualizer';
 import { configThemePlugin } from './theme';
-import { configImageminPlugin } from './imagemin';
 import { configSvgIconsPlugin } from './svgSprite';
 
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
   const {
-    VITE_USE_IMAGEMIN,
     VITE_USE_MOCK,
     VITE_LEGACY,
     VITE_BUILD_COMPRESS,
@@ -67,7 +65,7 @@ export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
   // The following plugins only work in the production environment
   if (isBuild) {
     // vite-plugin-imagemin
-    VITE_USE_IMAGEMIN && vitePlugins.push(configImageminPlugin());
+    // VITE_USE_IMAGEMIN && vitePlugins.push(configImageminPlugin());
 
     // rollup-plugin-gzip
     vitePlugins.push(
