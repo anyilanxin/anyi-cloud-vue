8589e45347c3ec98626094b472892d1eca23b43c
zxuanhong <z7630853@163.com>
2023-05-20 17:08:41 +0800

    feat(doc): change anyilanxin logo
    change anyilanxin logo

--- src/settings/siteSetting.ts
+++ src/settings/siteSetting.ts
@@ -1,8 +1,8 @@
 // github repo url
-export const GITHUB_URL = 'https://github.com/zxiaozhou/skillfull-cloud';
+export const GITHUB_URL = 'https://github.com/zxiaozhou/AnYi-Cloud';
 
 // vue-skill-full-next-doc
-export const DOC_URL = 'https://github.com/zxiaozhou/skillfull-cloud';
+export const DOC_URL = 'https://github.com/zxiaozhou/AnYi-Cloud';
 
 // site url
 export const SITE_URL = 'https://divisu.com';
